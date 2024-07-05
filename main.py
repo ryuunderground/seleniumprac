c258fec24c08f57ce34213c4f98e7879f2cb2c27
ryuunderground <jaeking00001@gmail.com>
2024-07-05 18:08:03 +0900

    title scrapper

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-KEYWORD = "Hololive"
-
 # 브라우저 꺼짐 방지 옵션
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -16,7 +14,7 @@ browser = webdriver.Chrome(options=chrome_options)
 browser.get("https://google.com")
 # 검색바 찾고 입력, 엔터
 search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
-search_bar.send_keys(KEYWORD)
+search_bar.send_keys("hololive")
 search_bar.send_keys(Keys.ENTER)
 #대기, 바로 검색하니까 차마 결과가 나오기 전에 결과를 모아버림
 time.sleep(5)
@@ -24,8 +22,8 @@ time.sleep(5)
 search_results = browser.find_elements(By.CLASS_NAME, "MjjYud")
 search_results.pop()
 #결과 추려내기, 제목만
-for index, search_result in enumerate(search_results):
-    search_result.screenshot(f"{KEYWORD} x {index}.png")
-
+for search_result in search_results:
+    title = search_result.find_element(By.TAG_NAME, "h3")
+    print(title.text)
 #브라우저 끄기
-#browser.quit()
+browser.quit()
