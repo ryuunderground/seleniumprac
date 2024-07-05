749d1c882d264bab10faa159b734785ff3211488
ryuunderground <jaeking00001@gmail.com>
2024-07-06 05:01:47 +0900

    from python to JS

--- main.py
+++ main.py
@@ -20,22 +20,36 @@ browser.get("https://google.com")
 search_bar = browser.find_element(By.CLASS_NAME, "gLFyf")
 search_bar.send_keys(KEYWORD)
 search_bar.send_keys(Keys.ENTER)
-#대기 후 모으기, 바로 검색하니까 차마 결과가 나오기 전에 결과를 모아버림
-search_results = WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "TzHB6b.cLjAic.K7khPe")))
+    
+# 예외 처리
+search_result_exceptions = WebDriverWait(browser, 5).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "TzHB6b.Hwkikb.WY0eLb")))
+browser.execute_script("""
+const exception = arguments[0];
+for(let i = 0; i<exception.length; i++){
+    parent = exception[i].parentElement
+    if (parent !== null){            
+        while(parent.firstChild !== null){
+            parent.removeChild(parent.firstChild)
+        }
+    }
+}                
+""",
+    search_result_exceptions,
+)
 
-"""
-#time.sleep(5)
+#대기 후 모으기, 바로 검색하니까 차마 결과가 나오기 전에 결과를 모아버림
+time.sleep(5)
 #결과 모으기
-#search_results = browser.find_elements(By.CLASS_NAME, "MjjYud")
-"""
+search_results = browser.find_elements(By.CLASS_NAME, "TzHB6b")
 search_results.pop()
 #결과 추려내기, 제목만
 for index, search_result in enumerate(search_results):
+    search_result.screenshot(f"screenshots/{KEYWORD} x {index}.png")
     #제외하고 싶은 결과가 있을 경우
     #class_name = search_result.get_attribute("class")
-    """if "exception class name" not in class_name:
+    """
+    if "exception class name" not in class_name:
         search_result.screenshot(f"screenshots/{KEYWORD} x {index}.png")
     """
-    search_result.screenshot(f"screenshots/{KEYWORD} x {index}.png")
 #브라우저 끄기
-browser.quit()
+#browser.quit()
